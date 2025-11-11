9a79dbb955f4464fb60e4521f995a902e9ca7c03
AGawas <arnavgawas707@gmail.com>
2025-11-11 14:28:36 -0500

    Add struct architecture with two multiplexers

--- DM74LS15.vhd
+++ DM74LS15.vhd
@@ -82,4 +82,22 @@ begin
 
 end architecture behv;
 
+architecture struct of DM74LS153 is
+    component MUX4
+        port (
+            A, B, G  : in  std_logic;
+            C        : in  std_logic_vector(3 downto 0);
+            Y        : out std_logic
+        );
+    end component;
+begin
+    -- First multiplexer
+    MUX1: MUX4 port map (A => A, B => B, G => G1, C => C1, Y => Y1);
+
+    -- Second multiplexer
+    MUX2: MUX4 port map (A => A, B => B, G => G2, C => C2, Y => Y2);
+end architecture struct;
+
+
+
 
