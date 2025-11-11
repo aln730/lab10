6c221b6e57cef8b75186a798b733d2bddf07fe10
AGawas <arnavgawas707@gmail.com>
2025-11-11 14:28:15 -0500

    Add behavioral architecture for DM74LS153

--- DM74LS15.vhd
+++ DM74LS15.vhd
@@ -46,4 +46,40 @@ begin
               '0'   when others;  -- disabled or undefined
 end architecture df;
 
+architecture behv of DM74LS153 is
+begin
+
+    process (A, B, G1, C1)
+    begin
+        if G1 = '0' then  -- enabled
+            case (B & A) is
+                when "00" => Y1 <= C1(0) after 22 ns;
+                when "01" => Y1 <= C1(1) after 22 ns;
+                when "10" => Y1 <= C1(2) after 22 ns;
+                when "11" => Y1 <= C1(3) after 22 ns;
+                when others => Y1 <= '0' after 22 ns;
+            end case;
+        else
+            Y1 <= '0' after 22 ns;  -- disabled
+        end if;
+    end process;
+
+
+    process (A, B, G2, C2)
+    begin
+        if G2 = '0' then  -- enabled
+            case (B & A) is
+                when "00" => Y2 <= C2(0) after 22 ns;
+                when "01" => Y2 <= C2(1) after 22 ns;
+                when "10" => Y2 <= C2(2) after 22 ns;
+                when "11" => Y2 <= C2(3) after 22 ns;
+                when others => Y2 <= '0' after 22 ns;
+            end case;
+        else
+            Y2 <= '0' after 22 ns;  -- disabled
+        end if;
+    end process;
+
+end architecture behv;
+
 
