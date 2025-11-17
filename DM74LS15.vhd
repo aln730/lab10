9df21eac0933ddfed9d19b5033641ab5f76c2ab6
aln730 <arnavgawas707@gmail.com>
2025-11-17 16:35:47 -0500

    HELP ME

--- DM74LS15.vhd
+++ DM74LS15.vhd
@@ -1,90 +1,81 @@
-library ieee;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
 
 entity DM74LS153 is
     port (
-        -- Select inputs
-        A   : in  std_logic;
-        B   : in  std_logic;
-        
-        -- Enable inputs (active low)
-        G1  : in  std_logic;
-        G2  : in  std_logic;
-        
-        -- Data inputs for each multiplexer
-        C1  : in  std_logic_vector(3 downto 0);
-        C2  : in  std_logic_vector(3 downto 0);
-        
-        -- Outputs
-        Y1  : out std_logic;
-        Y2  : out std_logic
+        G1, G2 : in  std_logic;                  -- Enable for MUX1 and MUX2
+        A, B   : in  std_logic;                  -- Selection inputs
+        C1, C2 : in  std_logic_vector(3 downto 0); -- Data inputs for MUX1 and MUX2
+        Y1, Y2 : out std_logic                   -- Outputs for MUX1 and MUX2
     );
 end entity DM74LS153;
 
-----------------------------------------------------------------------
--- Dataflow architecture
-----------------------------------------------------------------------
 architecture df of DM74LS153 is
-    -- Internal control signals
-    signal sControl1, sControl2 : std_logic_vector(2 downto 0);
+    signal sControl1, sControl2 : std_logic_vector(3 downto 0);
 begin
-    -- Create control signals for each multiplexer (G, B, A)
-    sControl1 <= G1 & B & A;
-    sControl2 <= G2 & B & A;
+    -- Control signals for MUX1 and MUX2 (active-low enable)
+    sControl1 <= "0001" when A='0' and B='0' else
+                 "0010" when A='1' and B='0' else
+                 "0100" when A='0' and B='1' else
+                 "1000";
 
-    -- Model of the first multiplexer
+    sControl2 <= "0001" when A='0' and B='0' else
+                 "0010" when A='1' and B='0' else
+                 "0100" when A='0' and B='1' else
+                 "1000";
+
+    -- First multiplexer
     with sControl1 select
-        Y1 <= C1(0) when "000",
-              C1(1) when "001",
-              C1(2) when "010",
-              C1(3) when "011",
-              '0'   when others;  -- disabled or undefined
+        Y1 <= C1(0) when "0001",
+              C1(1) when "0010",
+              C1(2) when "0100",
+              C1(3) when "1000",
+              '0'   when others;
 
-    -- Model of the second multiplexer
+    -- Second multiplexer
     with sControl2 select
-        Y2 <= C2(0) when "000",
-              C2(1) when "001",
-              C2(2) when "010",
-              C2(3) when "011",
-              '0'   when others;  -- disabled or undefined
+        Y2 <= C2(0) when "0001",
+              C2(1) when "0010",
+              C2(2) when "0100",
+              C2(3) when "1000",
+              '0'   when others;
 end architecture df;
 
-----------------------------------------------------------------------
--- Behavioral architecture (using sControl1 and sControl2)
-----------------------------------------------------------------------
 architecture behv of DM74LS153 is
-    signal sControl1, sControl2 : std_logic_vector(2 downto 0);
 begin
-    sControl1 <= G1 & B & A;
-    sControl2 <= G2 & B & A;
-
-    process (sControl1, C1)
+    -- Process for first multiplexer
+    process(G1, A, B, C1)
     begin
-        case sControl1 is
-            when "000" => Y1 <= C1(0) after 22 ns;
-            when "001" => Y1 <= C1(1) after 22 ns;
-            when "010" => Y1 <= C1(2) after 22 ns;
-            when "011" => Y1 <= C1(3) after 22 ns;
-            when others => Y1 <= '0' after 22 ns;  -- disabled
-        end case;
+        if G1 = '1' then
+            Y1 <= '0' after 22 ns;
+        else
+            case (A & B) is
+                when "00" => Y1 <= C1(0) after 22 ns;
+                when "01" => Y1 <= C1(1) after 22 ns;
+                when "10" => Y1 <= C1(2) after 22 ns;
+                when "11" => Y1 <= C1(3) after 22 ns;
+                when others => Y1 <= '0' after 22 ns;
+            end case;
+        end if;
     end process;
 
-    process (sControl2, C2)
+    -- Process for second multiplexer
+    process(G2, A, B, C2)
     begin
-        case sControl2 is
-            when "000" => Y2 <= C2(0) after 22 ns;
-            when "001" => Y2 <= C2(1) after 22 ns;
-            when "010" => Y2 <= C2(2) after 22 ns;
-            when "011" => Y2 <= C2(3) after 22 ns;
-            when others => Y2 <= '0' after 22 ns;  -- disabled
-        end case;
+        if G2 = '1' then
+            Y2 <= '0' after 22 ns;
+        else
+            case (A & B) is
+                when "00" => Y2 <= C2(0) after 22 ns;
+                when "01" => Y2 <= C2(1) after 22 ns;
+                when "10" => Y2 <= C2(2) after 22 ns;
+                when "11" => Y2 <= C2(3) after 22 ns;
+                when others => Y2 <= '0' after 22 ns;
+            end case;
+        end if;
     end process;
-
 end architecture behv;
 
-----------------------------------------------------------------------
--- Structural architecture
-----------------------------------------------------------------------
 architecture struct of DM74LS153 is
     component MUX4
         port (
@@ -93,10 +84,25 @@ architecture struct of DM74LS153 is
             Y        : out std_logic
         );
     end component;
-begin
-    -- First multiplexer
-    MUX1: MUX4 port map (A => A, B => B, G => G1, C => C1, Y => Y1);
 
-    -- Second multiplexer
-    MUX2: MUX4 port map (A => A, B => B, G => G2, C => C2, Y => Y2);
+begin
+    -- Instantiate first MUX
+    MUX1_INST: MUX4
+        port map (
+            A => A,
+            B => B,
+            G => G1,
+            C => C1,
+            Y => Y1
+        );
+
+    -- Instantiate second MUX
+    MUX2_INST: MUX4
+        port map (
+            A => A,
+            B => B,
+            G => G2,
+            C => C2,
+            Y => Y2
+        );
 end architecture struct;
