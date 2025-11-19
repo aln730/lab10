ade58eed6b577d38122931a97b2ee60cb97defd7
AGawas <arnavgawas707@gmail.com>
2025-11-19 00:05:57 -0500

    Update INV.vhd

--- INV.vhd
+++ INV.vhd
@@ -1,9 +1,15 @@
+--  Inverter Gate (INV)
 entity INV is
-    port (A : in std_logic; Y : out std_logic);
+    port (
+        A : in  std_logic;   -- Input signal
+        Y : out std_logic    -- Output = NOT A
+    );
 end entity INV;
 
+--  Behavioral architecture with propagation delay
 architecture behv of INV is
 begin
+    -- Output Y is the logical NOT of input A
+    -- Propagation delay modeled as 4 ns
     Y <= not A after 4 ns;
 end architecture behv;
-
